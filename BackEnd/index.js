4c20efba18b29b477f412edb81253f0e139efb64
jehadalhorany <alhoranyjehad@gmail.com>
2022-10-22 02:24:46 +0300

    add finshing

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -27,3 +27,53 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, (() => {
     console.log(`server on ${PORT}`);
 }))
+
+
+/*
+routes
+
+- add user 
+http://localhost:5000/user/
+post
+-for test in post man
+
+{
+"email":"jehad",
+"img":"123"
+}
+
+-add job
+http://localhost:5000/grop/grop
+post
+-for test in post man
+
+{"gropjob":"Develoments ",
+"ActualCost":50,
+"TotalBudget":48,
+"BudgetVal":-2
+}
+
+-add sub job
+http://localhost:5000/grop/sub
+post
+-for test in post man
+
+{"grop":"Modules",
+"ActualCostsub":17,
+"TotalBudgetsub":21,
+"BudgetValsub":4,
+"grop_name":3       here can  1-Application  2-Develoments 3-Testing
+}
+
+add users in job or sub
+http://localhost:5000/jopUser
+post-get
+-for test in post man
+
+{
+    "grop_id": 3,    here can 1-Application  2-Develoments 3-Testing
+    "user_id":1,     here can 1-jehad  2-ahmad 
+    "job_id": 8      here can -Test Cases-Regression Testing-New Feature-Components-Pages-Modules-Planning-UI Design
+}
+
+*/
